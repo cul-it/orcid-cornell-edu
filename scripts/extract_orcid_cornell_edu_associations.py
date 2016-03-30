031fbdad3924f2002b0cf193e41d59587f85a0df
Simeon Warner <simeon.warner@cornell.edu>
2016-03-30 13:41:31 -0400

    Add --activity option

--- scripts/extract_orcid_cornell_edu_associations.py
+++ scripts/extract_orcid_cornell_edu_associations.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Script to extract data from log produced by orcid.cornell.edu app."""
+from datetime import datetime
 import optparse
 import logging
 import re
@@ -7,22 +8,33 @@ import glob
 from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import Namespace, NamespaceManager, RDF
 
-def read_log(logfile, assocs):
+def read_log(logfile, assocs, activity):
     """Read Success.log written by orcid.cornell.edu app, flag any errors or mismatches.
 
     Reads from logfile and adds new associations to assocs. Since data is accumulated in
     assocs it makes most sense to read from a set of log files in the sequence they were
     created, so that any warnings follow time sequence.
+
+    Records activity by date into the date indexed dict activity. Records activity only
+    the first time we have seen a given netid.
     """
     fh = open(logfile,'r')
     for line in fh:
-        m =re.search(r'''result=SUCCESS, loginId=(\w+), orcidId=((\d\d\d\d-){3}\d\d\d[\dX])''',line)
+        m =re.search(r'''^(\d+\-\d+\-\d+).*result=SUCCESS, loginId=(\w+), orcidId=((\d\d\d\d-){3}\d\d\d[\dX])''',line)
         if (m):
-            netid = m.group(1)
-            orcid = m.group(2)
+            date = m.group(1)
+            netid = m.group(2)
+            orcid = m.group(3)
+            # Do we already have an entry for this day?
+            if (date not in activity):
+                activity[date] = 0
             # Have we seen this before, if so is it same or change?
-            if (netid in assocs and assocs[netid] != orcid):
-                loggin.warn("Changed ORCID for %s: %s -> %s, taking last" % (netid,assocs[netid],orcid))
+            if (netid in assocs):
+                if (assocs[netid] != orcid):
+                    loggin.warn("Changed ORCID for %s: %s -> %s, taking last" % (netid,assocs[netid],orcid))
+            else:
+                # Record only new netid as activity
+                activity[date] += 1
             assocs[netid] = orcid
         else:
             logging.warn("Ignoring bad line %s"%(line))
@@ -52,18 +64,30 @@ p.add_option('--success-log', action='store', default='Success.log',
              help="Read success log file (default: %default)")
 p.add_option('--outfile', action='store', default='netid_orcid_associations.nt',
              help="Output ntriples file (default: %default)")
+p.add_option('--activity', action='store', default=None,
+             help="Output file for activity data (default: %default)")
 (opts, args) = p.parse_args()
 
 # Read all logs
 assocs = {}
+activity = {}
 if (opts.old_success_logs):
     for logfile in glob.glob(opts.old_success_logs):
-        read_log(logfile, assocs)
-read_log(opts.success_log, assocs)
+        read_log(logfile, assocs, activity)
+read_log(opts.success_log, assocs, activity)
 
 # Write data
 g = build_graph(assocs)
 fh = open(opts.outfile,'wb')
 fh.write(g.serialize(format='nt'))
 fh.close()
+
+# Write activity data if requested
+if (opts.activity):
+    fh = open(opts.activity,'w')
+    fh.write('# Cornell ORCID integration activity (as of %s)\n' % (datetime.now()))
+    fh.write('#date       new_associations\n')
+    for date in sorted(activity.keys()):
+        fh.write('%s  %d\n' % (date,activity[date]))
+    fh.close()
 
